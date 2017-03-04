641906bcae7e1536246574b52f70ff5e8531a420
leeluolee <87399126@163.com>
2017-03-05 01:27:18 +0800

    add testcase for #162 pr

--- test/spec/browser-bugfix.js
+++ test/spec/browser-bugfix.js
@@ -865,6 +865,42 @@ it('bugfix #50', function(){
   })
 
 
+  it("bugfix-161: r-component  update ref ", function(){
+
+    var NameSpace = Regular.extend();
+    var container = document.createElement("div");
+    var Component1 = NameSpace.extend({
+      name: "c1",
+      template: "<p ref=p>c1</p>"
+    })
+    var Component2 = NameSpace.extend({
+      name: "c2",
+      template: "<p ref=p>c2</p>"
+    })
+
+    var component = new NameSpace({
+
+      data: {
+        modules: [1,2],
+        compName: 'c1'
+      },
+
+      template: 
+        '{#list modules as module}\
+            <r-component is={compName} ref="module-{module_index}"/>\
+        {/list}'
+    }).$inject(container);
+
+    expect(component.$refs['module-0'].$refs.p.innerHTML).to.equal('c1');
+    expect(component.$refs['module-1'].$refs.p.innerHTML).to.equal('c1');
+
+    component.$update('compName', 'c2')
+    
+    expect(component.$refs['module-0'].$refs.p.innerHTML).to.equal('c2');
+    expect(component.$refs['module-1'].$refs.p.innerHTML).to.equal('c2');
+  })
+
+
   // it("bug #122: paren Expression shouldn't change the set property it wrapped", function(){
   //   var Sub = Regular.extend({
   //     name: 'Sub',
